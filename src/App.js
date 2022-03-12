dbb36c5f05f36c3ac7f3640158cd789312b9e27a
Tohid Shabanloo <tshabanloo@Hotmail.com>
2022-03-12 18:52:50 +0330

    2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./Components/Header/Header";
-import Counter from "./Components/Counter/Counter";
-import Footer from "./Components/Footer/Footer";
-import Button from "./Components/Button/Button";
+import Header from "./Components/Header";
+import Counter from "./Components/Counter";
+import Footer from "./Components/Footer";
+import Button from "./Components/Button";
 
 function App() {
   return (
