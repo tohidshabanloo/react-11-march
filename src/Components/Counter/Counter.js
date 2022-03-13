ab5b701b3ea678d64e1abea51ab06fbf20845e98
Tohid Shabanloo <tshabanloo@Hotmail.com>
2022-03-13 19:34:17 +0330

    fix template

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,9 +1,13 @@
 import Button from "../Button/Button";
 import "./Counter.css";
+
 const Counter = () => {
   return (
     <div className="counter">
-      <Button />
+      <Button backgroundColor="black">+</Button>
+      <Button backgroundColor="black" style={{ marginLeft: 10 }}>
+        -
+      </Button>
     </div>
   );
 };
